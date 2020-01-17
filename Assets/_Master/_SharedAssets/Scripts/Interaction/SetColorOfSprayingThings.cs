6f510dc8c88afcc6ae29088ac5395aa3d0d9ccee
david lobser <davidlobser@yahoo.com>
2020-01-17 18:13:39 -0500

    init

---
+++ Assets/_Master/_SharedAssets/Scripts/Interaction/SetColorOfSprayingThings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Interactable_ChooseColor))]
+public class SetColorOfSprayingThings : MonoBehaviour
+{
+
+    public ParticleSystem particleSystem;
+    public Material material;
+    Interactable_ChooseColor chooseColor;
+    Color prevColor;
+
+    void Start()
+    {
+        chooseColor = GetComponent<Interactable_ChooseColor>();
+        SetColor();
+    }
+
+    void Update()
+    {
+
+        if(!prevColor.Equals(chooseColor.color)){
+            SetColor();
+        }
+        prevColor = chooseColor.color;
+    }
+
+    void SetColor(){
+        particleSystem.startColor = chooseColor.color;
+        material.color = chooseColor.color;
+    }
+}
